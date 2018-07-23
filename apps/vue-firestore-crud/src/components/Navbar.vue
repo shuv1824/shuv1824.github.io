c3e8b2fb2fabdbb0a4e67f2bffb04e090d1aa7c9
Shah Nawaz Shuvo <shahshuvo@yahoo.com>
2018-07-24 00:21:45 +0600

    Removing vue-firestore-crud

--- apps/vue-firestore-crud/src/components/Navbar.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <nav>
-    <div class="nav-wrapper grey">
-      <div class="container">
-        <router-link to="/" class="brand-logo">Employee Manager</router-link>
-      </div>
-    </div>
-  </nav>
-</template>
